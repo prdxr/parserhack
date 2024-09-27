b0e9765479ca63be5f0ce033f37f03d470916c53
neverelz <belyavtseva04@mail.ru>
2024-09-27 13:49:07 +0300

    proba

--- bot/app.py
+++ bot/app.py
@@ -10,3 +10,5 @@ from handlers.on_shutdown import shutdown
 if __name__ == "__main__":
     executor.start_polling(dp, on_startup=start, 
                            on_shutdown=shutdown)
+
+
--- bot/handlers/on_start.py
+++ bot/handlers/on_start.py
@@ -1,12 +1,12 @@
 import asyncio
-from jobs import send_new_events
+from bot.jobs import send_new_events
 from aiogram.types import BotCommand, BotCommandScopeDefault
-from loader import bot
+from bot.loader import bot
 from .admin import send_to_admin
 
 
 async def start(dp):
-    asyncio.create_task(send_new_events())  # периодическая задача для новых мероприятий
+    await asyncio.create_task(send_new_events())  # периодическая задача для новых мероприятий
     await bot.set_my_commands(
         commands=[
             BotCommand("events", "Открыть меню мероприятий"),
