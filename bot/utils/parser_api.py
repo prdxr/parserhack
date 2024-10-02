7863c46026bf3835de0a71106c6fa29983386b29
all0neee <Andaev.1@yandex.ru>
2024-10-02 14:02:28 +0300

    refactor: consolidate duplicated code for API GET-requests into a common function

--- bot/utils/parser_api.py
+++ bot/utils/parser_api.py
@@ -9,12 +9,25 @@ from models import *
 
 logger = logging.getLogger(f"root.{__name__}")
 _headers = {
-        "Authorization": "Token " + token
+    "Authorization": "Token " + token
 }
 API_BASE_URL = os.getenv("API_BASE_URL")
 if API_BASE_URL is None:
     sys.exit("Incorrect API url")
 
+
+async def get_request(url: str, model: type) -> list:
+    """
+    Функция для выолнения GET-запросов к API и парсинга полученных данных
+
+    :param url: URL для запроса
+    :param model: Модель для парсинга
+    :return: list[model]
+    """
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=url, headers=_headers) as response:
+            data = await response.json(content_type="application/json")
+            return [model.parse_obj(item) for item in data]
 
 def api_request_error_handler(func):
     async def wrapper(*args, **kwargs):
@@ -31,26 +44,13 @@ def api_request_error_handler(func):
 async def get_events_types() -> list[EventType]:
     url = API_BASE_URL + "hackaton/types/"
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url=url, headers=_headers) as response:
-            data = await response.json(content_type="application/json")
-            events_types = []
-            for raw_event_type in data:
-                events_types.append(EventType.parse_obj(raw_event_type))
-            return events_types
-        
+    return await get_request(url, EventType)
 
 @api_request_error_handler
 async def get_events() -> list[Event]:
     url = API_BASE_URL + "hackaton/"
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url=url, headers=_headers) as response:
-            data = await response.json(content_type="application/json")
-            events = []
-            for raw_event in data:
-                events.append(Event.parse_obj(raw_event))
-            return events
+    return await get_request(url, Event)
 
 
 @api_request_error_handler
@@ -62,29 +62,17 @@ async def get_events_by_preferences(events_types: list[int], tags: list[int]) ->
 
     for tag in tags:
         url += f"tags={tag}&"
-    
+
     url = url[:-1]
-    
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url=url, headers=_headers) as response:
-            data = await response.json(content_type="application/json")
-            events = []
-            for raw_event in data:
-                events.append(Event.parse_obj(raw_event))
-            return events
+
+    return await get_request(url, Event)
 
 
 @api_request_error_handler
 async def get_tags() -> list[EventTag]:
     url = API_BASE_URL + "hackaton/tags/"
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url=url, headers=_headers) as response:
-            data = await response.json(content_type="application/json")
-            tags = []
-            for raw_tag in data:
-                tags.append(EventTag.parse_obj(raw_tag))
-            return tags
+    return await get_request(url, EventTag)
 
 
 @api_request_error_handler
@@ -94,11 +82,5 @@ async def get_updates() -> list[Event]:
     Используется для рассылки
     """
     url = API_BASE_URL + "hackaton/update/"
-    
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url=url, headers=_headers) as response:
-            data = await response.json(content_type="application/json")        
-            events = []
-            for raw_event in data:
-                events.append(Event.parse_obj(raw_event))
-            return events
+
+    return await get_request(url,Event)
