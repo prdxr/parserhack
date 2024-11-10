73ccbfddcbd997c997b5b2e996e8e35a3e7086bf
Tim Orlov <crazy.paradoxer@gmail.com>
2024-11-10 16:55:01 +0300

    chore: start tg mini-app branch, add upcoming tg_miniapp container directory

---
+++ tg_miniapp/app.py
@@ -0,0 +1,9 @@
+from aiogram import executor
+from handlers.users import *
+from handlers.on_start import start
+from handlers.on_shutdown import shutdown
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, on_startup=start, 
+                           on_shutdown=shutdown)
---
+++ tg_miniapp/login.py
@@ -0,0 +1,31 @@
+import os
+import requests
+import sys
+from loader import BASE_DIR
+from dotenv import load_dotenv
+
+load_dotenv((BASE_DIR / ".env").as_posix())
+
+API_BASE_URL = os.getenv("API_BASE_URL")
+if API_BASE_URL is None:
+    sys.exit("Incorrect API url")
+
+
+def get_token() -> str:
+    data = {
+        "username": os.getenv("USERNAME"),
+        "password": os.getenv("PASSWORD")
+    }
+    response = requests.post(url=API_BASE_URL + "auth/token/login/",
+                             data=data)
+
+    if response.status_code == 400:
+        requests.post(url=API_BASE_URL + "auth/users/",
+                      data=data)
+        response = requests.post(url=API_BASE_URL + "auth/token/login/", 
+                                 data=data)
+        json_data = response.json()
+        return json_data["auth_token"]
+    else:
+        json_data = response.json()
+        return json_data["auth_token"]
