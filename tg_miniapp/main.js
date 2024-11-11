ebbe834851da8a5223692ae7425a5c5e92dcf12f
Tim Orlov <crazy.paradoxer@gmail.com>
2024-11-11 03:02:00 +0300

    feat: add miniapp-bot

---
+++ tg_miniapp/main.js
@@ -0,0 +1,19 @@
+import { Telegraf, Markup } from "telegraf";
+import { message } from "telegraf/filters";
+import "dotenv/config";
+
+const token = process.env.TOKEN;
+const webapp_url = process.env.WEB_APP_URL;
+
+const bot = new Telegraf(token);
+
+bot.command("start", (ctx) => {
+    ctx.reply(
+        "Добро пожаловать! Нажмите на кнопку ниже, чтобы войти в приложение.",
+        Markup.keyboard([
+            Markup.button.webApp("Войти в Parserhack", `${webapp_url}`),
+        ])
+    )
+});
+
+bot.launch();
