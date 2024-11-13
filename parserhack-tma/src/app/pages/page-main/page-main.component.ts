686e668beb6b7a1fd586f36fd2fa430d11e07a2f
Tim Orlov <crazy.paradoxer@gmail.com>
2024-11-13 19:44:31 +0300

    feat: add Ansible website subproject

---
+++ parserhack-tma/src/app/pages/page-main/page-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-main.component.html',
+  styleUrl: './page-main.component.scss'
+})
+export class PageMainComponent {
+
+}
