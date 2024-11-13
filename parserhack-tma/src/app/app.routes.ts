7b6519400a499e19ea75f15dfe71d97ff6deac37
Tim Orlov <crazy.paradoxer@gmail.com>
2024-11-13 19:44:31 +0300

    feat: add Ansible website subproject

---
+++ parserhack-tma/src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import {PageMainComponent} from './pages/page-main/page-main.component';
+import {PageEventsComponent} from './pages/page-events/page-events.component';
+import {PageSettingsComponent} from './pages/page-settings/page-settings.component';
+
+export const routes: Routes = [
+  {
+    path: 'main',
+    component: PageMainComponent
+  },
+  {
+    path: 'events',
+    component: PageEventsComponent
+  },
+  {
+    path: 'settings',
+    component: PageSettingsComponent
+  }
+];
---
+++ parserhack-tma/src/app/pages/page-settings/page-settings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-settings',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-settings.component.html',
+  styleUrl: './page-settings.component.scss'
+})
+export class PageSettingsComponent {
+
+}
