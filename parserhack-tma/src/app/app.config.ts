05abd290274a5d3a8ccbdb519bc31e8327e3dd95
Tim Orlov <crazy.paradoxer@gmail.com>
2024-11-14 03:27:50 +0300

    feat: write service to allow api requests

--- parserhack-tma/src/app/app.config.ts
+++ parserhack-tma/src/app/app.config.ts
@@ -1,8 +1,48 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, Injectable, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {
+  HTTP_INTERCEPTORS,
+  HttpEvent, HttpHandler,
+  HttpHandlerFn, HttpInterceptor,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors, withInterceptorsFromDi
+} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(request: HttpRequest<any>, handler: HttpHandler): Observable<HttpEvent<any>> {
+    const token = 'ea2cf2f4d0406654a06d23eb9a8524e2d414e3fe';
+    const update = {
+      headers: request.headers.append('Authorization', `Token ${token}`)
+    };
+    const requestNew = request.clone(update);
+    console.log("TEST");
+    return handler.handle(requestNew);
+  }
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(
+      // DI-based interceptors must be explicitly enabled.
+      withInterceptorsFromDi(),
+    ),
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+  ]
 };
+
+// export function authInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
+//   const token = 'ea2cf2f4d0406654a06d23eb9a8524e2d414e3fe';
+//   const update = {
+//     headers: request.headers.append('Authorization', `Token ${token}`)
+//   };
+//   const requestNew = request.clone(update);
+//   console.log("TEST");
+//   return next(requestNew);
+// }
