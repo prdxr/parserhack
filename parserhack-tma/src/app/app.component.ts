9ff4807e5e9f16dd4182a603653a0a5d1bea76c3
Tim Orlov <crazy.paradoxer@gmail.com>
2024-11-14 03:27:50 +0300

    feat: write service to allow api requests

--- parserhack-tma/src/app/app.component.ts
+++ parserhack-tma/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- parserhack-tma/src/app/services/api.service.ts
+++ parserhack-tma/src/app/services/api.service.ts
@@ -1,9 +1,54 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+export interface IApiRow {
+  id: number;
+  tags: string[];
+  type: object;
+  title: string;
+  description: string;
+  addresses: string;
+  start_date: string;
+  registration_deadline: string;
+  end_date: string;
+  url: string;
+  is_free: boolean;
+}
+
+export type TApiData = IApiRow[];
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  // public apiEndpoint = "http://88.218.67.139:83";
+  public apiEndpoint = "https://ph-api-gw.temp.kitt-club.ru/api"
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  public authToken: string = '';
+
+  public initialize(): any {
+    const authEndpoint = this.apiEndpoint + '/auth/token/login/';
+    return this.http.post(this.apiEndpoint, JSON.stringify(
+      {
+        "username": "root",
+        "password": "root"
+      }
+    ));
+  }
+
+  public getData(): Observable<TApiData> {
+    const action = this.apiEndpoint + '/hackaton/';
+    const headers = {
+      // "Authorization": "Token ea2cf2f4d0406654a06d23eb9a8524e2d414e3fe"
+    };
+    return this.http.get<any[]>(action, {headers});
+  }
+
 }
