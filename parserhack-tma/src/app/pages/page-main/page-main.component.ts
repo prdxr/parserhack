c7446702e33fe4b963234096f79fa76d576fb7c4
Tim Orlov <crazy.paradoxer@gmail.com>
2024-11-14 03:29:31 +0300

    feat: set up main page stub

--- parserhack-tma/src/app/pages/page-main/page-main.component.ts
+++ parserhack-tma/src/app/pages/page-main/page-main.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import {NgForOf} from '@angular/common';
+import {ApiService, TApiData} from '../../services/api.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-page-main',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './page-main.component.html',
   styleUrl: './page-main.component.scss'
 })
 export class PageMainComponent {
+
+  public user = 'tim'; // PLACEHOLDER
+  public data: TApiData = [];
+
+  constructor(
+    public apiService: ApiService
+  ) {
+    setTimeout(() => {
+      this.getData();
+    }, 1000);
+  }
+
+  public getData() {
+    this.apiService.authToken = this.apiService.initialize();
+    console.log("INITIALIZING API Endpoint, TOKEN: ", this.apiService.authToken);
+    this.apiService.getData().subscribe(data => {
+      this.data = data;
+    })
+  }
 
 }
