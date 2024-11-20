e61e58c3337859130fb5086e9685e08b68dbd46c
Tim Orlov <crazy.paradoxer@gmail.com>
2024-11-20 09:39:38 +0300

    feat: add login and register pages

--- parserhack-tma/src/app/app.routes.ts
+++ parserhack-tma/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import {PageMainComponent} from './pages/page-main/page-main.component';
 import {PageEventsComponent} from './pages/page-events/page-events.component';
 import {PageSettingsComponent} from './pages/page-settings/page-settings.component';
+import {PageRegisterComponent} from './pages/page-register/page-register.component';
+import {PageLoginComponent} from './pages/page-login/page-login.component';
 
 export const routes: Routes = [
   {
@@ -20,5 +22,13 @@ export const routes: Routes = [
   {
     path: 'settings',
     component: PageSettingsComponent
+  },
+  {
+    path: 'register',
+    component: PageRegisterComponent
+  },
+  {
+    path: 'login',
+    component: PageLoginComponent
   }
 ];
