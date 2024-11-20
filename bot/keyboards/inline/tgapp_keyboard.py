594c8ce3c53f4c9728cba45939f53aa56efc6b16
all0neee <Andaev.1@yandex.ru>
2024-11-20 15:13:59 +0300

    fix: fix keyboard for webapp

--- bot/keyboards/inline/tgapp_keyboard.py
+++ bot/keyboards/inline/tgapp_keyboard.py
@@ -1,9 +1,13 @@
 import os
 
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 
 def generate_miniapp_keyboard():
-    keyboard = InlineKeyboardMarkup(InlineKeyboardButton(text="Открыть телеграм-приложение"),
-                                    web_app=os.getenv('WEB_APP_URL'))
+    keyboard = InlineKeyboardMarkup()
+    url = os.getenv('WEB_APP_URL')
+    webAppInfo = WebAppInfo(url=url)
+    button = InlineKeyboardButton(text="Открыть телеграм-приложение",
+                                    web_app=webAppInfo)
+    keyboard.add(button)
     return keyboard
