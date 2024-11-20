66655b482bfdd7a93ed6467bc70f3b7f21046fa4
Tim Orlov <crazy.paradoxer@gmail.com>
2024-11-20 09:41:21 +0300

    feat: add login and register on settings page

--- parserhack-tma/src/app/pages/page-settings/page-settings.component.ts
+++ parserhack-tma/src/app/pages/page-settings/page-settings.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-page-settings',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './page-settings.component.html',
   styleUrl: './page-settings.component.scss'
 })
 export class PageSettingsComponent {
 
+  constructor(
+    public router: Router,
+    public authService: AuthService,
+  ) {
+  }
+
+  protected readonly AuthService = AuthService;
+
+  register() {
+    this.router.navigate(['register']);
+  }
 }
